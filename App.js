d245cb3db139586bf16284e206211fcb4a8765b2
praveen142003 <praveenk142003@gmail.com>
2025-01-16 16:56:09 +0530

    pokemon cards

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import PokemonCards from './components/PokemonCards';
 
 export default function App() {
+  //  const charmanderDetails = {
+  //       name : 'Charmander',
+  //       image : require("./assets/charmander.png"),
+  //       type : 'Fire',
+  //       hp : 39,
+  //       moves : ['Scratch','Ember' ,'Growl' ,'Leer'],
+  //       weaknesses : ['Water','Rock']
+  //   }
+  //   const squirtleData = {
+  //     name: "Squirtle",
+  //     image: require("./assets/squirtle.png"), 
+  //     type: "Water",
+  //     hp: 44,
+  //     moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+  //     weaknesses: ["Electric", "Grass"],
+  //   };
+  
+  //   const bulbasaurData = {
+  //     name: "Bulbasaur",
+  //     image: require("./assets/bulbasaur.png"),
+  //     type: "Grass",
+  //     hp: 45,
+  //     moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+  //     weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+  //   };
+  
+  //   const pikachuData = {
+  //     name: "Pikachu",
+  //     image: require("./assets/pikachu.png"), 
+  //     type: "Electric",
+  //     hp: 35,
+  //     moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+  //     weaknesses: ["Ground"],
+  //   };
+    const cardData = [
+      {
+        name : 'Charmander',
+        image : require("./assets/charmander.png"),
+        type : 'Fire',
+        hp : 39,
+        moves : ['Scratch','Ember' ,'Growl' ,'Leer'],
+        weaknesses : ['Water','Rock']
+    },
+    {
+      name: "Squirtle",
+      image: require("./assets/squirtle.png"), 
+      type: "Water",
+      hp: 44,
+      moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+      weaknesses: ["Electric", "Grass"],
+    },
+    {
+      name: "Bulbasaur",
+      image: require("./assets/bulbasaur.png"),
+      type: "Grass",
+      hp: 45,
+      moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+      weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+    },
+    {
+      name: "Pikachu",
+      image: require("./assets/pikachu.png"), 
+      type: "Electric",
+      hp: 35,
+      moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+      weaknesses: ["Ground"],
+    }
+    ]
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerContainer}>
+            <Text style={styles.headerText}>Pokemon Cards</Text>
+          </View>
+    <ScrollView>
+            <PokemonCards datas = {cardData} />
+            {/* <PokemonCards {...charmanderDetails}/>
+            <PokemonCards {...pikachuData}/>
+            <PokemonCards {...squirtleData}/>
+            <PokemonCards {...bulbasaurData}/> */}
+    </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  headerContainer : {
+    marginTop : 40,
+    backgroundColor :'cornflowerblue',
+    alignItems : 'center',
+    padding : 10
+  },
+  headerText : {
+    color : 'white',
+    fontWeight : 'bold'
+  }
 });
---
+++ components/PokemonCards.js
@@ -0,0 +1,121 @@
+import { FlatList, Image, Platform, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+
+function getTypeDetails(type) {
+  switch (type.toLowerCase()) {
+    case "electric":
+      return { borderColor: "#FFD700", emoji: "⚡️" };
+    case "water":
+      return { borderColor: "#6493EA", emoji: "💧" };
+    case "fire":
+      return { borderColor: "#FF5733", emoji: "🔥" };
+    case "grass":
+      return { borderColor: "#66CC66", emoji: "🌿" };
+    default:
+      return { borderColor: "#A0A0A0", emoji: "❓" };
+  }
+}
+export default function Cards({ datas }) {
+  const Deatils = (item) => {
+    const { borderColor, emoji } = getTypeDetails(item.type)
+    return (
+      <View style={styles.cardContainer}>
+        <View style={styles.nameContainer}>
+          <Text style={{ fontWeight: 'bold',color:'cornflowerblue' ,fontSize:22 }}>{item.name}</Text>
+          <Text style={{ fontWeight: 'bold' }}>❤️HP:{item.hp}</Text>
+        </View>
+        <Image source={item.image} resizeMode='contain' style={styles.image} />
+        <View style={styles.typeDetails}>
+          <View style={[styles.badge, { borderColor }]}>
+            <Text style={styles.batchEmoji}>{emoji}</Text>
+            <Text style={styles.batchType}>{item.type}</Text>
+          </View>
+        </View>
+        <View style={styles.movesContainer}>
+          <Text style={styles.movesText}>Moves :<Text>{item.moves.join(',')}</Text></Text>
+        </View>
+        <View style={styles.weaknessContainer}>
+          <Text style={styles.weaknessText}>Weakness :<Text>{item.weaknesses.join(',')}</Text></Text>
+        </View>
+      </View>
+    )
+  }
+
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={datas}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          Deatils(item)
+        )}
+      />
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    margintop: 60
+  },
+  cardContainer: {
+    backgroundColor: 'lightgrey',
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: '#333',
+        shadowOpacity: 0.4,
+        shadowRadius: 4
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  },
+  nameContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    marginBottom: 16
+  },
+  typeDetails: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 40,
+  },
+  badge: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 20,
+    borderWidth: 4,
+  },
+  batchEmoji: {
+    paddingRight: 5
+  },
+  movesContainer: {
+    marginBottom: 16
+  },
+  movesText: {
+    fontSize: 15,
+    fontWeight: 'bold'
+  },
+  weaknessContainer: {
+    mrginbottom: 30
+  },
+  weaknessText: {
+    fontSize: 15,
+    fontWeight: 'bold'
+  }
+})
